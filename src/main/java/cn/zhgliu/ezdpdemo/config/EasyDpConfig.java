f23c3a9116c2994fb1f55f204ca6c4e6b91b93a4
zhgliu <zhiguangliu1982@gmail.com>
2022-04-25 00:59:06 +0800

    init

--- src/main/java/cn/zhgliu/ezdpdemo/config/EasyDpConfig.java
+++ src/main/java/cn/zhgliu/ezdpdemo/config/EasyDpConfig.java
@@ -1,17 +1,55 @@
 package cn.zhgliu.ezdpdemo.config;
 
+import cn.zhgliu.ezdp.client.DataPermClient;
+import cn.zhgliu.ezdp.client.impl.BaseDataPermClient;
+import cn.zhgliu.ezdp.finder.DataPermMatchingModeFinder;
+import cn.zhgliu.ezdp.finder.DataPermRuleFinder;
+import cn.zhgliu.ezdp.finder.impl.http.HttpDataMatchingModeFinder;
+import cn.zhgliu.ezdp.finder.impl.http.HttpDataPermRuleFinder;
+import cn.zhgliu.ezdp.resolver.DataPermSqlResolver;
+import cn.zhgliu.ezdp.resolver.impl.ali.druid.DruidSQLResolver;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EasyDpConfig {
 
+    //    @Value()
+    private String dataPermServer="http://localhost:8899/ezdp";
+
+    //    @Value()
+    private String subSystem="SELLER_SYSTEM";
+
     @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    public MybatisPlusInterceptor mybatisPlusInterceptor(@Autowired DataPermClient dataPermClient) {
         MybatisPlusInterceptor i = new MybatisPlusInterceptor();
-        i.addInnerInterceptor(new MybatisPlusDataPermInterceptor());
+        i.addInnerInterceptor(new MybatisPlusDataPermInterceptor(dataPermClient));
         return i;
     }
+
+    @Bean
+    public DataPermMatchingModeFinder matchingModeFinder() {
+        return new HttpDataMatchingModeFinder(dataPermServer);
+    }
+
+    @Bean
+    public DataPermRuleFinder ruleFinder() {
+        return new HttpDataPermRuleFinder(dataPermServer);
+    }
+
+    @Bean
+    public DataPermSqlResolver resolver() {
+        return new DruidSQLResolver();
+    }
+
+    @Bean
+    public DataPermClient dataPermClient(@Autowired DataPermMatchingModeFinder matchingModeFinder,
+                                         @Autowired DataPermRuleFinder ruleFinder,
+                                         @Autowired DataPermSqlResolver resolver) {
+        return new BaseDataPermClient(matchingModeFinder, ruleFinder,resolver,subSystem);
+    }
+
 
 }
--- src/main/java/cn/zhgliu/ezdpdemo/config/MybatisPlusDataPermInterceptor.java
+++ src/main/java/cn/zhgliu/ezdpdemo/config/MybatisPlusDataPermInterceptor.java
@@ -1,22 +1,20 @@
 package cn.zhgliu.ezdpdemo.config;
 
 
+import cn.zhgliu.ezdp.client.DataPermClient;
+import cn.zhgliu.ezdp.helper.DataPermHelper;
 import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
-import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
-import org.springframework.stereotype.Component;
 
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Properties;
 
 // 获取执行的sql，获取sql的id，调用client，处理sql，将处理后的sql交还给mybatis执行
 @Intercepts(
@@ -26,52 +24,34 @@ import java.util.Properties;
         }
 )
 @Slf4j
-@Component
 public class MybatisPlusDataPermInterceptor implements InnerInterceptor {
 
-    public MybatisPlusDataPermInterceptor() {
-        System.out.println("=====================MybatisPlusDataPermInterceptor====================");
-    }
+    DataPermClient dataPermClient;
 
-    @Override
-    public boolean willDoQuery(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) throws SQLException {
-        System.out.println("=================================willDoQuery");
-        return false;
+    public MybatisPlusDataPermInterceptor(DataPermClient dataPermClient) {
+        this.dataPermClient = dataPermClient;
     }
 
     @Override
     public void beforeQuery(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) throws SQLException {
         System.out.println("=================================beforeQuery");
 
-    }
+        String sql = boundSql.getSql();
+        String userId = DataPermHelper.getUserId();
+        String statementCode = DataPermHelper.getStatementCode();
 
-    @Override
-    public boolean willDoUpdate(Executor executor, MappedStatement ms, Object parameter) throws SQLException {
-        System.out.println("=================================willDoUpdate");
-        return false;
-    }
+        String id = ms.getId();
+        System.out.println(sql);
+        System.out.println(userId);
+        System.out.println(statementCode);
+        System.out.println(id);
+        if (id.equals(statementCode)) {
 
-    @Override
-    public void beforeUpdate(Executor executor, MappedStatement ms, Object parameter) throws SQLException {
-        System.out.println("=================================beforeUpdate");
+            dataPermClient.addPermissionCondition(sql, userId, statementCode);
 
-    }
 
-    @Override
-    public void beforePrepare(StatementHandler sh, Connection connection, Integer transactionTimeout) {
-        System.out.println("=================================beforePrepare");
+        }
 
     }
 
-    @Override
-    public void beforeGetBoundSql(StatementHandler sh) {
-        System.out.println("=================================beforeGetBoundSql");
-
-    }
-
-    @Override
-    public void setProperties(Properties properties) {
-        System.out.println("=================================setProperties");
-
-    }
 }
